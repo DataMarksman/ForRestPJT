7dbc7cffc5d79ba943d8c750ff3962a1e4f419c1
ganjisriver <ganjisriver18@gmail.com>
2022-11-16 15:01:14 +0900

    good

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    user_profile_img = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+
