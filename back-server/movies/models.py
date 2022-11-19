4a826ec7a969075955e7e90bd4f20defcaf4a093
DataMarksman <zkstizn@gmail.com>
2022-11-19 18:25:56 +0900

    Back_end_modeling 7/10, views 4/12, urls 8/10 completed

--- back-server/movies/models.py
+++ back-server/movies/models.py
@@ -2,23 +2,15 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-# class Article(models.Model):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=100)
-#     content = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-
-# class Comment(models.Model):
-#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-#     content = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
 
 # https://image.tmdb.org/t/p/w500/c6OLXfKAk5BKeR6broC8pYiCquX.jpg
 
 POSTER_BASIC_URL = "https://image.tmdb.org/t/p/w500/"
+
+# 장르 속성 먼저 던져놓기 (무비의 다대다 필드로 쓸 예정)
+class Genre(models.Model):
+    name = models.TextField()
+
 
 # 영화 모델
 class Movie(models.Model):
@@ -30,21 +22,22 @@ class Movie(models.Model):
     popularity = models.FloatField(null=True)
     # 포스터 디폴트 값은 임시로 그 여자 작사 그 남자 작곡 
     poster_path = models.TextField(default="https://image.tmdb.org/t/p/w500/d9C2H1qoFt9AL4DwRlqEEZK4hVa.jpg")
-    
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_movies", symmetrical=True)
 
 
-# 댓글 모델
+# 이 부분 혹시 리뷰 -> 댓글 형식으로 추가하고 싶윽시면 말씀 주세욥. 그 부분 소스로 따로 짜놨습니다.
+# 댓글 모델 (유저, 영화를 외래키로 쓰고 / 제목, 내용을 받아서 )
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    content = models.TextField()
+    title = models.TextField(max_length=50)
+    content = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_comment", symmetrical=True)
 
-# class Movie_Info(models.Model):
-#     tmdb_id = models.CharField(max_length=20)
-#     title = models.CharField(max_length=50)
-#     overview = models.TextField(blank=True)
-#     release_date = models.CharField(max_length=50)
-#     popularity = models.IntegerField(null=True)
-#     poster_path = models.TextField(default="d9C2H1qoFt9AL4DwRlqEEZK4hVa.jpg")
+
+
+
+
+
