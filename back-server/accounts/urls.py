2d9c6a8780d34a0188113839af0aa8179da1355d
DataMarksman <zkstizn@gmail.com>
2022-11-21 05:24:01 +0900

    user,movie like/profile

--- back-server/accounts/urls.py
+++ back-server/accounts/urls.py
@@ -5,7 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     # 유저 관련 기능: 프로필 열람 및 수정, 팔로잉/언팔
-    path('profile/<username>/', views.profile),
+    path('profile/<username>/', views.profile_or_edit),
     path('profile/<username>/follow/', views.follow),
     
 ]
